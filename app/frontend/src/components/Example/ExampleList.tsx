cdd26054864bf28fdf577affde9e143e80a9957c
ramvemireddy <32643813+ramvemireddy@users.noreply.github.com>
2023-12-09 01:54:53 +0000

    pages cupdated

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
+        text: "What is NJTA?",
         value: "What is included in my Northwind Health Plus plan that is not in standard?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "What are role and responsibilities of Software Development manager for NJTA proposal?", value: "Role and responsibilities of Software Development manager" },
+    { text: "What does a Scrum Master do?", value: "Role and responsibilities of a scrum master?" }
 ];
 
 interface Props {
